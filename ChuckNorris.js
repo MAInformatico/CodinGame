5ac336799bd2267f89ad6b273f8871aabcc1d497
Miguel Angel Gutiérrez <magutierrezsistemas@gmail.com>
2018-11-21 19:24:33 +0100

    Create ChuckNorris.js

---
+++ ChuckNorris.js
@@ -0,0 +1,23 @@
+
+const MESSAGE = readline();
+
+var res = "";
+var pre = -1;
+
+for (var i = 0; i < MESSAGE.length; i++) {
+
+    for (var j = 6; j >= 0; j--) {
+
+        var bit = MESSAGE.charCodeAt(i) >> j & 1;
+        if (bit !== pre) {
+
+            if (-1 !== pre) {
+                res+= " ";
+            }
+            res+= 1 == bit ? "0 " : "00 ";
+            pre = bit;
+        }
+        res+= "0";
+    }
+}
+print(res);
